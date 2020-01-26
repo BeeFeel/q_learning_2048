98fb8d389ffca66d66aae81cd799ca9d16571503
新井 敬太 <ary_4201@hotmail.co.jp>
2020-01-26 14:13:45 +0900

    ゲームオーバー判定などのバグ修正

--- game/game.py
+++ game/game.py
@@ -1,6 +1,7 @@
 from game.utils import Direction
 from enum import Enum
 import random
+import itertools
 
 class Game():
     X_SIZE = 4
@@ -8,10 +9,12 @@ class Game():
     INIT_NUM = 1
     EMPTY = 0
 
-    def __init__(self):
+    def __init__(self, is_print=False):
         self.board = [[0] * Game.X_SIZE for i in range(Game.Y_SIZE)]
         self._move_num = 0
-
+        self._is_print = is_print
+        
+        # ゲーム板の初期化
         self.board[0][0] = Game.INIT_NUM
 
     def move(self, direction):
@@ -27,13 +30,17 @@ class Game():
                 self._move_one(x, y, direction)
 
         if self._is_gameover():
-            print("GAME OVER")
-            print("MAX: {}".format(self._get_maxnum()))
-            print("Num of Move: {}".format(self._move_num))
+            if self._is_print:
+                print("GAME OVER")
+                print("MAX: {}".format(self._get_maxnum()))
+                print("Num of Move: {}".format(self._move_num))
+            return False
 
         self._random_set()
         self._move_num += 1
-        self.print()
+        if self._is_print:
+            self.print()
+        return True
 
     def print(self):
         for bb in self.board:
@@ -67,11 +74,21 @@ class Game():
             y = random.randrange(Game.Y_SIZE)
             i += 1
 
-        self.board[y][x] = Game.INIT_NUM
+        if i < 100:
+            self.board[y][x] = Game.INIT_NUM
 
     def _is_gameover(self):
-        for b in self.board:
-            if Game.EMPTY in b:
+        def _can_move(board, x, y):
+            for direction in Direction:
+                dx, dy = Direction.to_2axiz(direction)
+                if x+dx < 0 or y+dy < 0 or x+dx >= Game.X_SIZE or y+dy >= Game.Y_SIZE:
+                    continue
+                if board[y+dy][x+dx] == Game.EMPTY or board[y+dy][x+dx] == board[y][x]:
+                    #print("{}+{}, {}+{}:".format(x, dx, y, dy), board[y+dy][x+dx], board[y][x])
+                    return True
+            return False
+        for x, y in itertools.product(range(Game.X_SIZE), range(Game.Y_SIZE)):
+            if _can_move(self.board, x, y):
                 return False
         return True
 
